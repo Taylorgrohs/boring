8999f47817bbb07b6b981fcd77898e21624255a0
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-12 11:44:33 -0800

    Add highlight to text when you click it

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("#p1").click(function(){
+      $("#p1").removeClass();
+      $("#p1").addClass("p1-background");
+  });
+  $("#p2").click(function(){
+      $("#p2").removeClass();
+      $("#p2").addClass("p1-background");
+  });
+  $("#p3").click(function(){
+      $("#p3").removeClass();
+      $("#p3").addClass("p1-background");
+  });
+
+});
